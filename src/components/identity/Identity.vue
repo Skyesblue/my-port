9e39b0969b393b567df8c823ba8a00d3e68282e1
Ichell <rachellayu28@gmail.com>
2026-08-23 21:20:07 +0700

    First Look

---
+++ src/components/identity/Identity.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface propsInterface {
+  label: string;
+}
+
+const props = withDefaults(defineProps<propsInterface>(), {
+  label: '',
+});
+</script>
+
+<template>
+    <div class="identity-body">
+        <div class="identity-headline">
+          <h1 class="text-align-center m-0-bottom">{{ props.label }}</h1>
+          <p>The future is still under construction</p>
+        </div>
+    </div>
+</template>
